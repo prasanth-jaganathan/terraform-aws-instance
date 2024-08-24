381861bbad829676c4a323cad9095d317a9876cd
prasanth <67551199+prasanth-jaganathan@users.noreply.github.com>
2024-08-24 19:36:17 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,4 +9,13 @@ terraform {
 }
 module "test-module" {
    source = "./modules/ec2"  ## child module
+
+   instance_tags = {
+      Name = "Test-devops-instance"
+      Description = "This is a test instance overridden by module"
+   }
+   instances = {
+      dev = "t2.micro"
+   }
+   region = "us-east-2"
 }
