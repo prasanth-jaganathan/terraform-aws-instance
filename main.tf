76dd60b712862470cb2589e0a5ca574ec572bc6c
prasanth <67551199+prasanth-jaganathan@users.noreply.github.com>
2024-08-24 19:10:41 +0530

    Updated backend configuration

--- main.tf
+++ main.tf
@@ -1,4 +1,12 @@
 ## root module
+terraform {
+  backend "remote" {
+    organization = "prasanth_jagan"
+    workspaces  {
+       name = "terraform-aws-instance"
+    }
+  }
+}
 module "test-module" {
    source = "./modules/ec2"  ## child module
    instance_tags = {
